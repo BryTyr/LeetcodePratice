064112dedccde9e794e12104430919aa97746be4
Bryan Tyrrell <50870604+BryTyr@users.noreply.github.com>
2022-08-31 07:17:17 +0100

    Time: 14 ms (45.70%), Space: 43.3 MB (36.46%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -7,13 +7,16 @@ class Solution {
             char sChar = s.charAt(i);
             char tChar = t.charAt(i);
             
+            // if s letter already mapped, check mapping
             if(charMap.containsKey(sChar)){
                 if(charMap.get(sChar) != tChar)
                     return false;
             }
+            // else check if tChar has been mapped to a diff value for a new sChar
             else if(charSet.contains(tChar)){
                 return false;
             }
+            // complete new entry add
             else{
                 charMap.put(sChar, tChar);
                 charSet.add(tChar);
